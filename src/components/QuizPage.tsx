bb35b17dc7011354cf8461afbe2cc72b1a382242
Andrew Huntington <andrew.huntington@gmail.com>
2022-03-03 18:52:10 +0900

    Retrieve data from API and display on quiz page

--- src/components/QuizPage.tsx
+++ src/components/QuizPage.tsx
@@ -1,5 +1,31 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { nanoid } from 'nanoid';
+
+interface QuestionData {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
 
 export default function QuizPage() {
-  return <div>QuizPage</div>;
+  const [questions, setQuestions] = useState<QuestionData[]>([]);
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
+      .then((res) => res.json())
+      .then((data) => setQuestions(data.results));
+  }, []);
+
+  // TODO: Figure out how to convert HTML code to comprehendible text
+  const questionList = questions.map((question) => (
+    <div key={nanoid()}>
+      {console.log(question)}
+      <h2>{question.question}</h2>
+    </div>
+  ));
+
+  return <div>{questionList}</div>;
 }
